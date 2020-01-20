f541ba3ab0c81d96520db291b2d10bc273d8c84b
mei <mei@DESKTOP-BRGB3P4>
2020-01-20 19:00:22 +0800

    新改动

---
+++ src/main/java/test/demo/Test2.java
@@ -0,0 +1,18 @@
+package test.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+
+
+	@org.testng.annotations.Test
+	public void testCase1() {
+		System.out.println("123£¬case1");
+	}
+	@org.testng.annotations.Test
+	public void testCase2() {
+		System.out.println("123412412414£¬case2");
+	}
+
+}
